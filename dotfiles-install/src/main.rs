9a17835e52ba62ebfc73921f3130a663216b2dfa
Othi <mnpq.raven@gmail.com>
2023-05-11 11:59:52 +0700

    chore

--- dotfiles-install/src/main.rs
+++ dotfiles-install/src/main.rs
@@ -3,6 +3,7 @@ use clap::Parser;
 use dotfiles_schema::{ConfigFile, Task};
 use std::{env, error::Error};
 use tracing::info;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
 use worker::io::{get_path_type, PathType};
 
 mod worker;
@@ -17,6 +18,7 @@ struct Cli {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // INFO: logging
     let cli = Cli::parse();
 
     let trace_level = match cli.debug {
@@ -27,9 +29,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let file_appender = tracing_appender::rolling::never(path, "dotfile_install.log");
     // TODO: this hogs stdout
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let tracing_writer = non_blocking.and(std::io::stdout);
 
     tracing_subscriber::fmt()
-        .with_writer(non_blocking)
+        .with_writer(tracing_writer)
         .with_ansi(false)
         .with_max_level(trace_level)
         .init();
